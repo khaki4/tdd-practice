c494ff23cce8f690cdd06c474bc4b51ae5cf645c
정지원 <4dakto@gmail.com>
2017-11-25 20:35:50 +0900

    test들을 descibe block으로 묶고 when에 따라 다시한번 묶음

--- src/App.test.js
+++ src/App.test.js
@@ -2,20 +2,29 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import App from './App'
 
-const app = shallow(<App />)
-
-it('render correctly', () => {
-  expect(app).toMatchSnapshot()
-})
-
-it('init the `state` with an empty list of gifts', () => {
-  expect(app.state().gifts).toEqual([])
-})
-
-it('adds a new gift to `state` when clicking the `add gift` button', () => {
-  app.find('.btn-add').simulate('click')
+describe('App', () => {
+  const app = shallow(<App />)
   
-  expect(app.state().gifts).toEqual([
-      {id: 1}
-    ])
+  it('render correctly', () => {
+    expect(app).toMatchSnapshot()
+  })
+  
+  it('init the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([])
+  })
+  
+  describe('when clicking the `add-gift` button', () => {
+    beforeEach(() => {
+      app.find('.btn-add').simulate('click')
+    })
+    afterEach(() => {
+      app.setState({ gifts: [] })
+    })
+    it('adds a new gift to `state`', () => {
+      expect(app.state().gifts).toEqual([{id: 1}])
+    })
+    it('adds a new gift to the rendered list', () => {
+      expect(app.find('.gift-list').children().length).toEqual(1)
+    })
+  })
 })
